b2152ed86f9364665bd7b6955e33d3cebae70ccb
mostafafinal <mostafafinal55@gmail.com>
2025-02-15 16:03:58 +0200

    feat: use TMDB to fetch popular movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,54 @@
 import Search from './components/Search'
-import { useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import './App.css'
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`
+  }
+}
 
 function App() {
   const [searchItem, setSearchItem] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState('');
+  const [movieList, setMovieList] = useState([]);
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMsg('');
+  
+    try {
+      const endpoints = `${import.meta.env.VITE_TMDB_API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+  
+      const res = await fetch(endpoints, API_OPTIONS);
+  
+      if(!res.ok) throw new Error("Failed to fetch movies");
+  
+      const data = await res.json();
+  
+      if (data.Response === 'False') {
+        setErrorMsg(data.Error || 'Failed to fetch movies');
+        setMovieList([]);
+  
+        return
+      }
+  
+      setMovieList(data.results || []);
+    } catch (err) {
+      console.error(`Error fetching movies: ${err}`);
+  
+      setErrorMsg('Error fetching movies! Please try again later.')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies()
+  }, []);
 
   return (
     <main>
@@ -13,10 +58,27 @@ function App() {
           <header>
             <img src="./hero.png" alt="Hero Banner" />
             <h1>Find <span className="text-gradient">Movies</span> You&apos;ll Enjoy Without the Hassle</h1>
+
+            <Search searchItem={searchItem} setSearchItem={setSearchItem}/>
           </header>
 
-          <Search searchItem={searchItem} setSearchItem={setSearchItem}/>
-          <h1 className="text-white">{searchItem}</h1>
+          <section className='all-movies'>
+            <h2 className='mt-[40px]'>All Movies</h2>
+
+            {isLoading ? (
+              <p className="text-white">Loading...</p>
+            ) : errorMsg ? (
+              <p className="text-red-500">{errorMsg}</p>
+            ) : (
+              <ul>
+                {movieList.map(movie => (
+                  <Fragment key={movie.id}>
+                    <p className="text-white">{movie.title}</p>
+                  </Fragment>
+                ))}
+              </ul>
+            )}
+          </section>
         </div>
       </div>
     </main>
